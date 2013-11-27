a5d784b4fe1157e61b47b4febcdff68d96e1ce65
poupotte <bellot.zoe@gmail.com>
2013-11-27 17:27:48 +0100

    change path of source files

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ database="cozy-files"
 COUCH="http://localhost:5984"
 
 curl -X DELETE $COUCH/$database
-rm -rf /etc/cozy-files
+rm -rf /etc/cozy/cozy-files
+rm -rf /usr/local/cozy/cozy-files
 
 echo "\033[33m Database configuration ... \033[0m"
 curl -HContent-Type:application/json -vXPUT $COUCH/_users/org.couchdb.user:$username --data-binary "{\"_id\": \"org.couchdb.user:$username\",\"name\": \"$username\",\"roles\": [],\"type\": \"user\",\"password\": \"$password\"}"
@@ -15,28 +16,33 @@ echo "\033[32m Database configured \033[0m"
 
 
 echo "\033[33m Recover source ... \033[0m"
-mkdir /etc/cozy-files
-cd /etc/cozy-files
+user="$(users)"
+mkdir /etc/cozy
+mkdir /etc/cozy/cozy-files
+mkdir /usr/local/cozy
+mkdir /usr/local/cozy/cozy-files
+mkdir ~/cozy-files
+cd /usr/local/cozy/cozy-files
 git clone https://github.com/poupotte/couchdb-fuse.git
 echo "\033[32m Source recovered \033[0m"
 
 # Stocker password
 echo "\033[33m Password configuration ... \033[0m"
-touch /etc/cozy-files/couchdb.login
-user="$(users)"
-chown $user:$user /etc/cozy-files/couchdb.login
-chmod 700 /etc/cozy-files/couchdb.login
-echo "$username" >> /etc/cozy-files/couchdb.login
-echo "$password" >> /etc/cozy-files/couchdb.login
+touch /etc/cozy/cozy-files/couchdb.login
+chown $user:$user /etc/cozy/cozy-files/couchdb.login
+chmod 700 /etc/cozy/cozy-files/couchdb.login
+echo "$username" >> /etc/cozy/cozy-files/couchdb.login
+echo "$password" >> /etc/cozy/cozy-files/couchdb.login
 echo "\033[32m Password configured \033[0m"
 
-cd couchdb-fuse/
+cd /usr/local/cozy/cozy-files/couchdb-fuse/
 # Install dependencies
 echo "\033[33m Dependencies installation ... \033[0m"
 apt-get install python-fuse
 apt-get install python-couchdb
 apt-get install python-pip
 pip install requests
+python setup.py install
 apt-get install python-appindicator
 apt-get install python-glade2
 apt-get install python-gobject
@@ -47,8 +53,8 @@ echo "\033[33m Start cozy_files ... \033[0m"
 str='/DEAMON_OPT=""/ a\DAEMONUSER="'
 str="$str$user"
 str="$str\""
-sed -i "$str" /etc/cozy-files/couchdb-fuse/cozy_files
-cp /etc/cozy-files/couchdb-fuse/cozy_files /etc/init.d
+sed -i "$str" /usr/local/cozy/cozy-files/couchdb-fuse/cozy_files
+cp /usr/local/cozy/cozy-files/couchdb-fuse/cozy_files /etc/init.d
 chmod 733 /etc/init.d/cozy_files
 /etc/init.d/cozy_files start
 update-rc.d cozy_files defaults
